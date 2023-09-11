49da2928be03008071cf3a949951548e5967ee4e
Ishola Ayomide <101745279+Ay4codes@users.noreply.github.com>
2023-09-11 15:49:43 +0100

    UTC time format

--- src/controllers/userCtrl.js
+++ src/controllers/userCtrl.js
@@ -6,6 +6,7 @@ class userCtrl {
             slack_name: req.query?.slack_name ? req.query.slack_name : 'Ayomide Ishola',
             current_day: timeZone.dayOfWeek(Number(timeZone.weekday())),
             // Used luxon to get utc time because the server utc time is 10sec slower to the grader requirement
+            // My getUtcFunc --> timezone.getCurrentUTC()
             utc_time: timeZone.getCurrentUtcWithLuxon(),
             track: req.query?.track ? req.query.track : 'backend',
             github_file_url: 'https://github.com/Ay4codes/hng-stage-one-task/blob/main/src/controllers/userCtrl.js',
